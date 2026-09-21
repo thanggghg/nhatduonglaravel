7e81f8468b7eb3c88ec10fd5893a9953d53b5baf
ThangLe <34125726+thanggghg@users.noreply.github.com>
2026-09-21 20:54:22 +0700

    Refresh site favicon assets

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -3,7 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/png" href="{{ asset('Nhat-Duong-Logo-1-768x543.png') }}">
+    <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
     <title>@yield('title', 'Dashboard') - Admin Panel</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @stack('styles')
--- resources/views/home-new.blade.php
+++ resources/views/home-new.blade.php
@@ -21,8 +21,10 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <link rel="icon" type="image/png" href="{{ asset('Nhat-Duong-Logo-1-768x543.png') }}">
-  <link rel="apple-touch-icon" href="{{ asset('Nhat-Duong-Logo-1-768x543.png') }}">
+  <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
+  <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
+  <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
+  <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
   <title>{{ $seoTitle }}</title>
   <meta name="description" content="{{ $seoDescription }}">
   <link rel="canonical" href="{{ $homeUrls[$locale] }}">
